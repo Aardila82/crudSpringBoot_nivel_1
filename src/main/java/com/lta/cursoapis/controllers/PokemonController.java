afdb5a35c407785e99071f46bcc08cabe9f1790d
Alexander <desarrollador1@arsuarez.com>
2026-02-10 15:01:29 -0500

    Se realiza actualizacion en controller con ResponseEntity

--- src/main/java/com/lta/cursoapis/controllers/PokemonController.java
+++ src/main/java/com/lta/cursoapis/controllers/PokemonController.java
@@ -2,6 +2,7 @@ package com.lta.cursoapis.controllers;
 
 import com.lta.cursoapis.domain.Pokemon;
 import com.lta.cursoapis.service.PokemonService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,26 +18,29 @@ public class PokemonController {
     }
 
     @GetMapping
-    public List<Pokemon> getPokemon(){
-        return service.findAll();
+    public ResponseEntity<List<Pokemon>>  getPokemon(){
+        return ResponseEntity.ok(service.findAll());
     }
 
     @PostMapping
-    public Pokemon createPokemon(@RequestBody Pokemon pokemon) {
-        return service.create(pokemon);
+    public ResponseEntity<Pokemon> createPokemon(@RequestBody Pokemon pokemon) {
+        Pokemon created = service.create(pokemon);
+        return ResponseEntity.status(201).body(created);
     }
 
     @PutMapping("/{id}")
-    public Pokemon updatePokemon(
+    public ResponseEntity<Pokemon> updatePokemon(
             @PathVariable Integer id,
             @RequestBody Pokemon pokemon) {
 
-        return service.update(id, pokemon);
+        Pokemon updated = service.update(id, pokemon);
+        return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
-    public void deletePokemon(@PathVariable Integer id) {
+    public ResponseEntity<Void> deletePokemon(@PathVariable Integer id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
